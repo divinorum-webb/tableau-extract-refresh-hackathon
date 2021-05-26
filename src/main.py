6b5feaa943a038e277df485abd1461964f36e9d5
elliottstam <elliott.stam@gmail.com>
2021-05-27 00:34:28 +0200

    updated the demo code in main.py

--- src/main.py
+++ src/main.py
@@ -14,17 +14,6 @@ def main():
     response = extract_runner.unpause_workbook(workbook_name="Hackathon May2021")
     print(response)
 
-    # extract_runner = ExtractRefreshTaskManager(conn=tableau_connection)
-    # response = extract_runner.pause_datasource(datasource_name="BigQuery Extract1")
-    # response = extract_runner.unpause_datasource(datasource_name="BigQuery Extract1")
-    # print(response)
-
-    # schedule_runner = ScheduleManager(conn=tableau_connection)
-    # response = schedule_runner.pause_schedule(schedule_name="Devyx Test Weekly")
-    # print(response)
-    # response = schedule_runner.unpause_schedule(schedule_name="Devyx Test Weekly")
-    # print(response)
-
     tableau_connection.sign_out()
 
 
